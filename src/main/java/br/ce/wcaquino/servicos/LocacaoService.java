bdf835eef4175332f5a43fc564f8449f37897a3c
Maressavo <maressavargasoliveira@gmail.com>
2023-12-21 15:11:01 -0300

    Refatorando
    Refatoração para organizar os arquivos de teste, parte 1.

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,9 +7,6 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
-import org.junit.Assert;
-import org.junit.Test;
 
 /*
 	Os códigos aqui apresentados são parte do curso Testes unitários em JAVA, replicados aqui com o objetivo de aperfeiçoamento
@@ -33,26 +30,5 @@ public class LocacaoService {
 		//TODO adicionar método para salvar
 		
 		return locacao;
-	}
-
-	//public static void main(String[] args) {
-	@Test
-	public void test(){
-		//cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Joana");
-		Filme filme = new Filme("A volta dos que não foram", 2, 7.85);
-
-		//acao
-		Locacao locacao = service.alugarFilme(usuario,filme);
-
-		//verificacao
-
-		//System.out.println(locacao.getValor());
-		Assert.assertTrue(locacao.getValor() == 7.85);
-		//System.out.println(locacao.getDataLocacao());
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		//System.out.println(locacao.getDataRetorno());
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
---
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest {
+    //public static void main(String[] args) {
+    @Test
+    public void test(){
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Joana");
+        Filme filme = new Filme("A volta dos que não foram", 2, 7.85);
+
+        //acao
+        Locacao locacao = service.alugarFilme(usuario,filme);
+
+        //verificacao
+        Assert.assertTrue(locacao.getValor() == 7.85);
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
