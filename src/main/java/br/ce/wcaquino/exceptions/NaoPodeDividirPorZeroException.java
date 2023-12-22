e01740fa00495b22a2f2cccfbb908bd2aa007b38
Maressavo <maressavargasoliveira@gmail.com>
2023-12-22 11:44:27 -0300

    Calculadora
    Início de TDD

---
+++ src/main/java/br/ce/wcaquino/exceptions/NaoPodeDividirPorZeroException.java
@@ -0,0 +1,5 @@
+package br.ce.wcaquino.exceptions;
+
+public class NaoPodeDividirPorZeroException extends Exception{
+    private static final long serialVersionUID = 3837982804180390821L;
+}
---
+++ src/test/java/br/ce/wcaquino/service/CalculadoraTest.java
@@ -0,0 +1,75 @@
+package br.ce.wcaquino.service;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+import br.ce.wcaquino.servicos.Calculadora;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+    private Calculadora calc;
+
+    @Before
+    public void setup() {
+        calc = new Calculadora();
+    }
+
+    @Test
+    public void deveSomarDoisValores() {
+        //cenario
+        int a = 5, b = 3;
+
+        //acao
+        int resultado = calc.somar(a, b);
+
+        //verificacao
+        Assert.assertEquals(8, resultado);
+    }
+
+    @Test
+    public void deveSubtrairDoisValores() {
+        //cenario
+        int a = 5, b = 3;
+
+        //acao
+        int resultado = calc.subtrair(a, b);
+
+        //verificacao
+        Assert.assertEquals(2, resultado);
+    }
+
+    @Test
+    public void deveMultiplicarDoisValores() {
+        //cenario
+        int a = 5, b = 3;
+
+        //acao
+        int resultado = calc.multiplicar(a, b);
+
+        //verificacao
+        Assert.assertEquals(15, resultado);
+    }
+
+    @Test
+    public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException{
+        //cenario
+        int a = 15, b = 3;
+
+        //acao
+        int resultado = calc.dividir(a, b);
+
+        //verificacao
+        Assert.assertEquals(5, resultado);
+    }
+
+    @Test(expected = NaoPodeDividirPorZeroException.class)
+    public void deveLancarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+        //cenario
+        int a = 10, b = 0;
+
+        //acao
+        int resultado = calc.dividir(a, b);
+
+    }
+}
